fbce4e6079de97250caf3edc7e0bc8da79f054da
Dipen parmar <dipensavji@gmail.com>
2026-03-24 19:07:52 +0530

    chore(release): bump version to 0.1.2
    Align package version in pyproject.toml and __init__.py for upcoming release,
    following multi-channel notification feature implementation.

--- src/slack_notifier/__init__.py
+++ src/slack_notifier/__init__.py
@@ -1,5 +1,5 @@
 # src/slack_notifier/__init__.py
 from .notifier import SlackNotifier, NotificationLevel
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __all__ = ["SlackNotifier", "NotificationLevel"]
